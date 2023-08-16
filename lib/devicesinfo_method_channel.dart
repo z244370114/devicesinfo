733b2222552e45074a4c80f7e4aee564786c7f08
zhouyuan <z244370114@outlook.com>
2023-08-16 17:26:48 +0800

    1.添加图片；

--- lib/devicesinfo_method_channel.dart
+++ lib/devicesinfo_method_channel.dart
@@ -9,78 +9,65 @@ class MethodChannelDevicesinfo extends DevicesinfoPlatform {
   @visibleForTesting
   static const methodChannel = MethodChannel('devicesinfo');
 
-  @override
   static Future<void> initApp() async {
     await methodChannel.invokeMethod('initApp');
   }
 
-  @override
   static Future<String?> getDeviceInfos() async {
     var data = await methodChannel.invokeMethod('getDeviceInfos');
     return data;
   }
 
-  @override
   static Future<String?> getGeneralData() async {
     var data = await methodChannel.invokeMethod('getGeneralData');
     return data;
   }
 
-  @override
   static Future<String?> getAppListDataArmour() async {
     var data = await methodChannel.invokeMethod('getAppListDataArmour');
     return data;
   }
 
-  @override
   static Future<String?> getBatteryStatusData() async {
     var data = await methodChannel.invokeMethod('getBatteryStatusData');
     return data;
   }
 
-  @override
   static Future<String?> getContactDataArmour() async {
     var data = await methodChannel.invokeMethod('getContactDataArmour');
     return data;
   }
 
-  @override
   static Future<String?> getHardwareData() async {
     var data = await methodChannel.invokeMethod('getHardwareData');
     return data;
   }
 
-  @override
   static Future<String?> getLocationAddressData() async {
     var data = await methodChannel.invokeMethod('getLocationAddressData');
     return data;
   }
 
-  @override
   static Future<String?> getMediaFilesData() async {
     var data = await methodChannel.invokeMethod('getMediaFilesData');
     return data;
   }
 
-  @override
   static Future<String?> getNetWorkData() async {
     var data = await methodChannel.invokeMethod('getNetWorkData');
     return data;
   }
 
-  @override
   static Future<String?> getSimCardData() async {
     var data = await methodChannel.invokeMethod('getSimCardData');
     return data;
   }
 
-  @override
   static Future<String?> getOtherData() async {
     var data = await methodChannel.invokeMethod('getOtherData');
     return data;
   }
 
-  @override
   static Future<String?> getStorageData() async {
     var data = await methodChannel.invokeMethod('getStorageData');
     return data;
